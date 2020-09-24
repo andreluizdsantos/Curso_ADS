8621804b51f64bc11e52998cfc63d4b354c0cf71
André Luiz dos Santos <andreluizdsantos@gmail.com>
2020-09-24 20:07:19 -0300

    Create ex010.py

---
+++ python/ex/ex010.py
@@ -0,0 +1,10 @@
+valor = 4050
+if valor <= 1830.29:
+    valor -= valor * 0.08
+    print(valor)
+elif valor <= 3050.52:
+    valor -= valor * 0.09
+    print(valor)
+elif valor <= 6101.06:
+    valor -= valor * 0.11
+    print(valor)

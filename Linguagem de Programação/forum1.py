6af31d91e55b6432a1b4b14527d2ad5bcffee8c1
André Luiz dos Santos <andreluizdsantos@gmail.com>
2020-09-27 11:59:58 -0300

    Update forum1.py

--- Linguagem de Programação/forum1.py
+++ Linguagem de Programação/forum1.py
@@ -1,10 +1,10 @@
 # v: 1.02
 colec = []
-p = ['Inventário da Coleção', ' 1 - Adicionar item: ', ' 2 - Remover item: ', ' 3 - Exibir Coleção: ', ' 4 - Sair do inventário: ']
+p = ['Inventário da Coleção', ' 1 - Adicionar item: ', ' 2 - Remover item: ', ' 3 - Exibir Coleção: ', ' 4 - Sair do inventário: ', ' Opção: ', ' Inventário está vazio.', ' Opção invalida!']
 i = 0
 while True:
     print('', 30*'=', '\n |{:^28}|\n'.format(p[0]), 30*'=', '\n |{:<28}|\n |{:<28}|\n |{:<28}|\n |{:<28}|\n'.format(p[1], p[2], p[3], p[4]), 30*'=')
-    op = int(input(' Opção: '))
+    op = int(input(p[5]))
     if op == 1:
         add = input(p[1])
         colec.append(add)
@@ -19,8 +19,8 @@ while True:
             print(f' Item {i + 1}: {colec[i]}')
             i = i + 1
         if colec == []:
-            print(' Inventário está vazio.')
+            print(p[6])
     elif op == 4:
         break
     else:
-        print(' Opção invalida!')
+        print(p[7])

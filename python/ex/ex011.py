d7e6be8270fce296b553277b895ec9b1e00de78c
André Luiz dos Santos <andreluizdsantos@gmail.com>
2020-10-05 22:18:08 -0300

    Update ex011.py

--- python/ex/ex011.py
+++ python/ex/ex011.py
@@ -112,6 +112,18 @@ else:
 # – 25 até 30: Sobrepeso
 # – 30 até 40: Obesidade
 # – Acima de 40: Obesidade Mórbida
-peso = float(input('Digie seu peso: '))
+"""peso = float(input('Digie seu peso: '))
 altura = float(input('Digite sua altura: '))
-
+imc = peso / (altura ** 2)
+print(f'Seu IMC é: {imc:.1f}', end='')
+if imc < 18.5:
+    print(' você está Abaixo do Peso')
+elif 18.5 <= imc < 25:
+    print(' você está no Peso Ideal')
+elif 25 <= imc < 30:
+    print(' você está com Sobrepeso')
+elif 30 <= imc < 40:
+    print(' você está com Obesidade')
+else:
+    print(' você está com Obesidade Mórbida')
+"""

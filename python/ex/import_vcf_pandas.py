478d4b34e32acafb6977983f9ebb6f22741b8eb6
André Luiz dos Santos <andreluizdsantos@gmail.com>
2021-11-26 15:25:18 -0300

    Create import_vcf_pandas.py

---
+++ python/ex/import_vcf_pandas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import io
+import os
+import pandas as pd
+
+
+def read_vcf(path):
+    with open(path, 'r') as f:
+        lines = [l for l in f if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})

3fb2c0573bdeefbb0ad7c0a8ae072f624065014e
André Luiz dos Santos <andreluizdsantos@gmail.com>
2022-01-10 16:15:56 -0300

    ex68

--- python/ex/aula15ex68.py
+++ python/ex/aula15ex68.py
@@ -2,29 +2,30 @@
  O jogo só será interrompido quando o jogador perder, mostrando o total de
   vitórias consecutivas que ele conquistou no final do jogo."""
 from random import randint
-numc = randint(0,10)
-cont = result = 0
+cont = tot = 0
 print(10 *"=-=","\nVamos jogar par ou ímpar!")
 while True:
     numj = int(input("Digite um valor: "))
-    escolha = str(input("Digite [P/I]: ")).strip().lower()[0]
-    if escolha in 'p':
-        jog = 0
-        comp = 'ímpar'
-    else:
-        jog = 1
-        comp = 'par'
-    if (numj + numc) % 2 == 0:
-        result = 0
-    else:
-        result = 1
-    if result == jog:
-        print("Jogador venceu!")
-        print(f"O computador escolheu {comp} e jogou: {numc}")
-        cont += 1
-    else:
-        print("Computador venceu!")
-        break
-    print(10 * "=-=")
-print(f"Jogador venceu {cont} jogadas.")
-print(f"O computador escolheu {comp} e jogou: {numc}")
+    escolha = ' '
+    while escolha not in 'PI':
+        escolha = str(input("Digite [P/I]: ")).strip().upper()[0]
+    numc = randint(0, 10)
+    tot = numj + numc
+    print(f"O computador jogou: {numc} e o jogador jogou {numj}, total: {tot}", end= ' ')
+    print("que é Par!" if tot % 2 == 0 else "que é Ímpar!")
+    if escolha in 'P':
+        if tot % 2 == 0:
+            print("Jogador venceu!")
+            cont += 1
+        else:
+            print("Computador venceu!")
+            break
+    elif escolha in 'I':
+        if tot % 2 == 1:
+            print("Jogador venceu!")
+            cont += 1
+        else:
+            print("Computador venceu!")
+            break
+print(10 * "=-=")
+print(f"Fim de jogo! O jogador venceu {cont} jogadas.")
